039122ff3ae0b777255e904063486d9ce285e0c0
lrpe <lassep@protonmail.com>
2021-11-09 20:29:26 +0100

    Added Xml Docs.

--- src/JustIoC/JustContainer.cs
+++ src/JustIoC/JustContainer.cs
@@ -4,29 +4,52 @@ using System.Linq;
 
 namespace JustIoC
 {
+    /// <summary>
+    /// Provides a collection of services for dependency injection.
+    /// </summary>
     public class JustContainer
     {
         private IDictionary<Type, Type> _justServices;
         private IDictionary<Type, object> _justInstances;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JustContainer"/> class.
+        /// </summary>
         public JustContainer()
         {
             _justServices = new Dictionary<Type, Type>();
             _justInstances = new Dictionary<Type, object>();
         }
 
+        /// <summary>
+        /// Registers the given service of type <typeparamref name="TService"/> as a singleton.
+        /// </summary>
+        /// <typeparam name="TService">The type of service to register.</typeparam>
+        /// <returns>The same <see cref="JustContainer"/> reference, so that multiple calls can be chained.</returns>
         public JustContainer Add<TService>()
         {
             _justServices.Add(typeof(TService), typeof(TService));
             return this;
         }
 
+        /// <summary>
+        /// Registers the given service of type <typeparamref name="TService"/> with the implementation type
+        /// <typeparamref name="TImplementation"/> as a singleton.
+        /// </summary>
+        /// <typeparam name="TService">The type of service to register.</typeparam>
+        /// <typeparam name="TImplementation">The type of the implementing class of the service.</typeparam>
+        /// <returns>The same <see cref="JustContainer"/> reference, so that multiple calls can be chained.</returns>
         public JustContainer Add<TService, TImplementation>()
         {
             _justServices.Add(typeof(TService), typeof(TImplementation));
             return this;
         }
 
+        /// <summary>
+        /// Get a service of type <typeparamref name="TService"/> from the container.
+        /// </summary>
+        /// <typeparam name="TService">The type of service to get.</typeparam>
+        /// <returns>A service of type <typeparamref name="TService"/>.</returns>
         public TService Get<TService>()
             where TService : class
         {
