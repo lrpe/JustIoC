ae204bd24fde15abf658052c0e9b8724c87455ca
lrpe <lassep@protonmail.com>
2021-11-09 20:29:33 +0100

    Updated sample code.

--- sample/JustIoC.Sample/Program.cs
+++ sample/JustIoC.Sample/Program.cs
@@ -8,14 +8,23 @@ namespace JustIoC.Sample
         static void Main(string[] args)
         {
             var just = new JustContainer();
+            just.Add<NewService>();
+            just.Add<AnotherService>();
             just.Add<TestService>();
             just.Add<ITestService, OtherService>();
 
-            var testService = just.Get<TestService>();
-            testService.Foo();
+            //var testService = just.Get<TestService>();
+            //testService.Foo();
 
             var otherService = just.Get<ITestService>();
             otherService.Foo();
+
+            var test = just.Get<ITestService>();
+
+            if (otherService == test)
+            {
+                Console.WriteLine("Works!");
+            }
         }
     }
 }
--- sample/JustIoC.Sample/Services/OtherService.cs
+++ sample/JustIoC.Sample/Services/OtherService.cs
@@ -4,9 +4,16 @@ namespace JustIoC.Sample.Services
 {
     public class OtherService : ITestService
     {
+        public OtherService(NewService service, AnotherService srv)
+        {
+
+        }
+
         public void Foo()
         {
             Console.WriteLine("Other");
         }
     }
+
+    public class AnotherService { }
 }
