2b1b0cd477c2b196a961d72f95af78620149ea66
lrpe <lassep@protonmail.com>
2021-11-10 04:05:02 +0100

    Introduced a ServiceDescriptor class for service registrations.

--- src/JustIoC/JustContainer.cs
+++ src/JustIoC/JustContainer.cs
@@ -9,7 +9,7 @@ namespace JustIoC
     /// </summary>
     public class JustContainer
     {
-        private IDictionary<Type, Type> _justServices;
+        private IDictionary<Type, JustDescriptor> _justServices;
         private IDictionary<Type, object> _justInstances;
 
         /// <summary>
@@ -17,7 +17,7 @@ namespace JustIoC
         /// </summary>
         public JustContainer()
         {
-            _justServices = new Dictionary<Type, Type>();
+            _justServices = new Dictionary<Type, JustDescriptor>();
             _justInstances = new Dictionary<Type, object>();
         }
 
@@ -28,7 +28,8 @@ namespace JustIoC
         /// <returns>The same <see cref="JustContainer"/> reference, so that multiple calls can be chained.</returns>
         public JustContainer Add<TService>()
         {
-            _justServices.Add(typeof(TService), typeof(TService));
+            var descriptor = new JustDescriptor(typeof(TService), typeof(TService));
+            _justServices.Add(descriptor.ServiceType, descriptor);
             return this;
         }
 
@@ -41,7 +42,8 @@ namespace JustIoC
         /// <returns>The same <see cref="JustContainer"/> reference, so that multiple calls can be chained.</returns>
         public JustContainer Add<TService, TImplementation>()
         {
-            _justServices.Add(typeof(TService), typeof(TImplementation));
+            var descriptor = new JustDescriptor(typeof(TService), typeof(TImplementation));
+            _justServices.Add(descriptor.ServiceType, descriptor);
             return this;
         }
 
@@ -58,17 +60,17 @@ namespace JustIoC
 
         private object Get(Type serviceType)
         {
-            if (!_justServices.TryGetValue(serviceType, out Type implementationType))
+            if (!_justServices.TryGetValue(serviceType, out JustDescriptor descriptor))
             {
                 throw new Exception($"No service could be resolved for type {serviceType}");
             }
 
             if (!_justInstances.TryGetValue(serviceType, out object instance))
             {
-                var constructors = implementationType.GetConstructors();
+                var constructors = descriptor.ImplementationType.GetConstructors();
                 if (constructors.Length != 1)
                 {
-                    throw new Exception($"More than one public constructor found for type '{implementationType}'.");
+                    throw new Exception($"More than one public constructor found for type '{descriptor.ImplementationType}'.");
                 }
                 var parameters = constructors.Single().GetParameters();
                 object[] args = new object[parameters.Length];
@@ -77,7 +79,7 @@ namespace JustIoC
                     var paramInstance = Get(param.ParameterType);
                     args[param.Position] = paramInstance;
                 }
-                instance = Activator.CreateInstance(implementationType, args);
+                instance = Activator.CreateInstance(descriptor.ImplementationType, args);
                 _justInstances.Add(serviceType, instance);
                 return instance;
             }
---
+++ src/JustIoC/JustDescriptor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JustIoC
+{
+    /// <summary>
+    /// Describes a service registration.
+    /// </summary>
+    public class JustDescriptor
+    {
+        /// <summary>
+        /// Initializes a new instance of <see cref="JustDescriptor"/> with the given <paramref name="serviceType"/>.
+        /// </summary>
+        /// <param name="serviceType">The <see cref="Type"/> of the service.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="serviceType"/> is <c>null</c>.</exception>
+        public JustDescriptor(Type serviceType)
+        {
+            ServiceType = serviceType ?? throw new ArgumentNullException(nameof(serviceType));
+            ImplementationType = ServiceType;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="JustDescriptor"/> with the given <paramref name="serviceType"/>
+        /// and <paramref name="implementationType"/>.
+        /// </summary>
+        /// <param name="serviceType">The <see cref="Type"/> of the service.</param>
+        /// <param name="implementationType">The <see cref="Type"/> that implements the service.</param>
+        /// <exception cref="ArgumentNullException">
+        /// <paramref name="serviceType"/> or <paramref name="implementationType"/> is <c>null</c>.
+        /// </exception>
+        public JustDescriptor(Type serviceType, Type implementationType)
+        {
+            ServiceType = serviceType ?? throw new ArgumentNullException(nameof(serviceType));
+            ImplementationType = implementationType ?? throw new ArgumentNullException(nameof(implementationType));
+        }
+
+        /// <summary>
+        /// The <see cref="Type"/> of the service.
+        /// </summary>
+        public Type ServiceType { get; }
+
+        /// <summary>
+        /// The <see cref="Type"/> that implements the service.
+        /// </summary>
+        public Type ImplementationType { get; }
+    }
+}
